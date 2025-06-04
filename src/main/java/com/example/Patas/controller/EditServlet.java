488dc9983bdbc519516ab83a836e44008de1ecc3
tanaka_rua <tanaka.rua@alhinc.jp>
2025-06-04 16:01:33 +0900

    【Update】ServletとServiceとｈｔｍｌの更新

--- src/main/java/com/example/Patas/controller/EditServlet.java
+++ src/main/java/com/example/Patas/controller/EditServlet.java
@@ -1,4 +1,52 @@
 package com.example.Patas.controller;
 
+import com.example.Patas.controller.form.TaskForm;
+import com.example.Patas.service.EditService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 public class EditServlet {
+    @Autowired
+    EditService editService;
+
+    /*
+     *編集画面表示
+     */
+    @GetMapping("/edit/{id}")
+    public ModelAndView editTask(@PathVariable("id") Integer id){
+        ModelAndView mav = new ModelAndView();
+
+        TaskForm taskForm = new TaskForm();
+
+        TaskForm editData = editService.editTask(id);
+
+        mav.setViewName("/edit");
+        // selectしてきた編集対象の投稿データを保管
+        mav.addObject("formModel", editData);
+
+        return mav;
+    }
+
+    @PutMapping("/update/{id}")
+    public ModelAndView editTask(@PathVariable("id") Integer id,
+                                 @ModelAttribute("formModel") @Validated TaskForm task, BindingResult result){
+
+        if(result.hasErrors()){
+            ModelAndView mav = new ModelAndView();
+            mav.addObject("formModel", task);
+            mav.setViewName("/edit");
+            return mav;
+        }else {
+            task.setId(id);
+            editService.saveTask(task);
+
+            return new ModelAndView("redirect:/");
+        }
+    }
 }
--- src/main/java/com/example/Patas/service/EditService.java
+++ src/main/java/com/example/Patas/service/EditService.java
@@ -1,4 +1,55 @@
 package com.example.Patas.service;
 
+
+import com.example.Patas.controller.form.TaskForm;
+import com.example.Patas.repository.entity.Task;
+import com.example.Patas.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 public class EditService {
+    @Autowired
+    TaskRepository taskRepository;
+
+    public TaskForm editTask(Integer id) {
+
+        List<Task> results = new ArrayList<>();
+        results.add((Task) taskRepository.findById(id).orElse(null));
+        List<TaskForm> reports = setTaskForm(results);
+
+        return reports.get(0);
+    }
+
+    private List<TaskForm> setTaskForm(List<Task> results) {
+        List<TaskForm> tasks = new ArrayList<>();
+
+        for (int i = 0; i < results.size(); i++) {
+            TaskForm task = new TaskForm();
+            Task result = results.get(i);
+            task.setId(result.getId());
+            task.setContent(result.getContent());
+            task.setLimitDate(result.getCreatedDate());
+        }
+        return tasks;
+    }
+
+    public void saveTask(TaskForm reqTask) {
+        Task saveTask = setReportEntity(reqTask);
+        taskRepository.save(saveTask);
+    }
+
+    /*
+     * リクエストから取得した情報をEntityに設定
+     */
+    private Task setReportEntity(TaskForm reqTask) {
+        Task task = new Task();
+        task.setId(reqTask.getId());
+        task.setContent(reqTask.getContent());
+        task.setLimitDate((Timestamp) reqTask.getLimitDate());
+        return task;
+    }
 }
