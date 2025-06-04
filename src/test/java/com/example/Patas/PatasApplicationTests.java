7097c3c4579754c039a927e3f6fce4e9b624a771
nakano_keisuke <nakano.keisuke@alhinc.jp>
2025-06-04 10:39:14 +0900

    first commit

---
+++ src/test/java/com/example/Patas/PatasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Patas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
