83b76bd857e7cb6667aafc7d065cc208fadd9cbd
tanaka_rua <tanaka.rua@alhinc.jp>
2025-06-05 15:35:19 +0900

    【Update】編集機能の条件分岐の書き換え、削除機能の実装

--- src/main/java/com/example/Patas/controller/EditServlet.java
+++ src/main/java/com/example/Patas/controller/EditServlet.java
@@ -5,12 +5,17 @@ import com.example.Patas.service.EditService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class EditServlet {
@@ -20,38 +25,32 @@ public class EditServlet {
     /*
      *編集画面表示
      */
-    @GetMapping("/edit/1")
-    public ModelAndView editTask(){
+    @GetMapping("/edit/{id}")
+    public ModelAndView editTask(@PathVariable("id") String taskId,
+                                 RedirectAttributes redirectAttributes) {
         ModelAndView mav = new ModelAndView();
 
-//        if(result.hasErrors()){
-//            List<String> errorMessages = new ArrayList<String>();
-//            //resultからデフォルトのエラーメッセージを取得
-//            //入っているerrorsの分だけ回してListに詰める(今回は「不正なパラメータです」のみだが、増えた時を考えてリスト)
-//            for(ObjectError error : result.getAllErrors()){
-//                errorMessages.add(error.getDefaultMessage());
-//            }
-//            redirectAttributes.addFlashAttribute("errorMessages", errorMessages);
-//
-//            return new ModelAndView("redirect:/");
-//        }else{
+        if (taskId != null && taskId.matches("^[0-9]+$]")) {
             TaskForm taskForm = new TaskForm();
-
-            TaskForm editData = editService.editTask(1);
-
-            //if(editData != null) {
+            int id = Integer.parseInt(taskId);
+            TaskForm editData = editService.editTask(id);
+            if (editData != null) {
                 mav.setViewName("/edit");
                 //selectしてきた編集対象の投稿データを保管
                 mav.addObject("formModel", editData);
-//            }else{
-//                String error = "不正なパラメータです";
-//                redirectAttributes.addFlashAttribute("error", error);
-//                mav.setViewName("redirect:/");
-//            }
-
-            return mav;
-        //}
+            } else {
+                String error = "不正なパラメータです";
+                redirectAttributes.addFlashAttribute("error", error);
+                mav.setViewName("redirect:/");
+            }
+        } else {
+            String error = "不正なパラメータです";
+            redirectAttributes.addFlashAttribute("error", error);
+            mav.setViewName("redirect:/");
+        }
+        return mav;
     }
+
 
     @PostMapping("/update/{id}")
     public ModelAndView editTask(@PathVariable("id") Integer id,
