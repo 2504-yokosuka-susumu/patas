53f4f357b7e64739ac64670d602b51e1d03e797e
yokosuka_susumu <yokosuka.susumu@alhinc.jp>
2025-06-05 17:16:37 +0900

    [Add]ステータス変更
    ・Servlet
    ・Service

--- src/main/java/com/example/Patas/Controller/ChangeServlet.java
+++ src/main/java/com/example/Patas/Controller/ChangeServlet.java
@@ -1,4 +1,22 @@
 package com.example.Patas.controller;
 
+import com.example.Patas.controller.form.TaskForm;
+import com.example.Patas.service.ChangeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class ChangeServlet {
+    ChangeService changeService;
+
+    @PostMapping("/change")
+    public ModelAndView editTask(@ModelAttribute("formModel") TaskForm task) {
+        changeService.saveTask(task);
+        return new ModelAndView("redirect:/");
+    }
 }
---
+++ src/main/java/com/example/Patas/service/ChangeService.java
@@ -0,0 +1,37 @@
+package com.example.Patas.service;
+
+import com.example.Patas.controller.form.TaskForm;
+import com.example.Patas.repository.TaskRepository;
+import com.example.Patas.repository.entity.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class ChangeService {
+    @Autowired
+    TaskRepository taskRepository;
+
+    /*
+     * レコード変更
+     */
+    public void saveTask(TaskForm reqTask) {
+        Task saveTask = setTaskEntity(reqTask);
+        taskRepository.save(saveTask);
+    }
+
+    /*
+     * リクエストから取得した情報をEntityに設定
+     */
+    private Task setTaskEntity(TaskForm reqTask) {
+        Task task = new Task();
+        task.setId(reqTask.getId());
+        task.setContent(reqTask.getContent());
+        task.setStatus(reqTask.getStatus());
+        task.setLimitDate(reqTask.getLimitDate());
+        task.setCreatedDate(reqTask.getCreatedDate());
+        task.setUpdatedDate(new Date());
+        return task;
+    }
+}
